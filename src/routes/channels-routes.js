011d19415930a57a66c8812e2df0ca06d4345677
IrexFA <65744041+IrexFA@users.noreply.github.com>
2024-12-05 18:29:18 +0100

    Added paging to channels

--- src/routes/channels-routes.js
+++ src/routes/channels-routes.js
@@ -6,9 +6,41 @@ const prisma = new PrismaClient();
 const channelsRouter = express.Router();
 
 channelsRouter.get('/', async (req, res) => {
-    const channels = await prisma.channels.findMany();
-    res.json(channels);
-})
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = 100;
+
+        if (page < 1) {
+            return res.status(400).json({ error: 'Le paramètre page doit être un entier positif.' });
+        }
+
+        const skip = (page - 1) * limit;
+
+        const channels = await prisma.channels.findMany({
+            skip: skip,
+            take: limit,
+            orderBy: {
+                followers_count: "desc"
+            }
+        });
+
+        const totalChannels = await prisma.channels.count();
+
+        const totalPages = Math.ceil(totalChannels / limit);
+
+        res.json({
+            data: channels,
+            meta: {
+                totalItems: totalChannels,
+                totalPages: totalPages,
+                currentPage: page,
+                perPage: limit,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la récupération des chaînes' });
+    }
+});
 
 channelsRouter.get('/:id', async (req, res) => {
     const { id } = req.params;
@@ -40,4 +72,5 @@ channelsRouter.get('/:id', async (req, res) => {
         streams: channelStreams
     });
 });
+
 module.exports = channelsRouter;
